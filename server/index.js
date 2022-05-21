01f4a108befb824da0b50b07f76e8656b16e0e32
Ashish Raghuvanshi <thetypo36@gmail.com>
2022-05-21 15:37:09 +0530

    set express mongodb and server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,26 @@
 //importing dependencies
 import express from "express";
 import cors from "cors";
-import parser from "body-parser";
+import bodyParser from "body-parser";
 import mongoose from "mongoose";
+
+const PORT = 5000;
+const CONNECTION_URL =
+  "mongodb+srv://thetypo36:36'ashish@cluster0.fn30s.mongodb.net/?retryWrites=true&w=majority";
+
+const app = express();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`server is running on the port: ${PORT}`)
+    )
+  )
+  .catch((error) => console.log(error.message));
