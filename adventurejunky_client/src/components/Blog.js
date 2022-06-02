2b210933da8559aca24ee4e9db699d0096ae8593
Ashish Raghuvanshi <thetypo36@gmail.com>
2022-06-02 23:04:10 +0530

    add menu and delete function for each blog

--- adventurejunky_client/src/components/Blog.js
+++ adventurejunky_client/src/components/Blog.js
@@ -2,11 +2,85 @@ import React from "react";
 
 class Blog extends React.Component {
   render() {
-    const data = this.props.data;
-    console.log("data", data);
+    const { data, key, OnClickDelete } = this.props;
+    let optionVisible = false;
+    // console.log("data", data);
+    const commentClickHandler = (event) => {
+      const blog = event.target.parentElement.parentElement.parentElement;
+
+      data.comments.comment.map((comment) => {
+        console.log("blog", blog);
+        return (blog.innerHTML += `<div class="comment">
+                <div class="comment-user">
+                ${comment.user}
+                </div>
+                <div class="comment-body">
+                ${comment.body}
+                </div>
+                <div>`);
+      });
+    };
+
+    const handleOptionClick = (event) => {
+      const reallyTarget = event.target.parentElement.lastChild;
+      if (optionVisible) {
+        reallyTarget.style.display = "none";
+        reallyTarget.style.border = "none";
+        reallyTarget.style.backgroundColor = "transparent";
+      } else {
+        const styles = {
+          display: "block",
+          border: "#FFFFFF",
+          backgroundColor: "#FFFFFF",
+        };
+        console.log(event.target.parentElement.lastChild.style);
+        Object.assign(event.target.parentElement.lastChild.style, styles);
+      }
+      optionVisible = !optionVisible;
+    };
     return (
       <div className="Blog">
-        <div className="blog-main-heading">{data.title} </div>
+        <div className="blog-main-heading">
+          {data.title}
+          <span className="options">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/70/70339.png"
+              alt="options"
+              onClick={handleOptionClick}
+            />
+            <div className="optionList">
+              <ul>
+                <li>
+                  <div className="OptionContainer">
+                    <img
+                      src="https://cdn-icons.flaticon.com/png/512/2099/premium/2099085.png?token=exp=1654185068~hmac=320a548aa069cc22f011355eaffc3480"
+                      alt="share"
+                    />
+                  </div>
+                  <div>Share</div>
+                </li>
+                <li>
+                  <div className="OptionContainer">
+                    <img
+                      src="https://cdn-icons-png.flaticon.com/512/1159/1159633.png"
+                      alt="edit"
+                    />
+                  </div>
+                  <div>Edit</div>
+                </li>
+                <li onClick={() => OnClickDelete(data.blogId)}>
+                  <div className="OptionContainer">
+                    <img
+                      src="https://cdn-icons.flaticon.com/png/512/3405/premium/3405244.png?token=exp=1654184961~hmac=8a7dc794a6ea5bdfcbb7c283fb33bfb7"
+                      alt="Delete"
+                    />
+                  </div>
+                  <div>Delete</div>
+                </li>
+              </ul>
+            </div>
+          </span>
+        </div>
         <div className="Blog-Details">
           By <span className="author-name">{data.author}</span>
           <span className="Creation-time"> {data.time}</span>
@@ -24,13 +98,16 @@ class Blog extends React.Component {
             />
             <span>{data.like}</span>
           </div>
-          <div className="dislike-btn">
+          <div className="comment-btn">
             <img
-              src="https://cdn-icons-png.flaticon.com/512/126/126504.png"
+              src="https://cdn-icons-png.flaticon.com/512/13/13673.png"
               width="12px"
-              alt="dislike-btn"
+              alt="comment-btn"
+              onClick={commentClickHandler}
             />
-            <span>{data.dislike}</span>
+            <span className="comment-counter">
+              {data.comments.commentCount}
+            </span>
           </div>
         </div>
       </div>
--- adventurejunky_client/src/components/BlogPage.js
+++ adventurejunky_client/src/components/BlogPage.js
@@ -3,60 +3,126 @@ import Blog from "./Blog";
 class BlogPage extends React.Component {
   constructor() {
     super();
-    this.state = [
-      {
-        title: "How to Escape world with a good Trip?",
-        author: "Anand Raghuvanshi",
-        like: 1122,
-        comments: [],
-        time: "29 May, 2022",
-        body: "So, as we all are born in this, adventure circle of 7 Hell, i.e is our life. Travelling can be proved as experincing havean.",
-        img: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201506/thumb-image_647_060515020243.jpg?size=770:433",
-      },
-      {
-        title: "How to Escape world with a good Trip?",
-        Author: "Anand Raghuvanshi",
-        like: 1122,
-        comments: [],
-        time: "29 May, 2022",
-        body: "So, as we all are born in this, adventure circle of 7 Hell, i.e is our life. Travelling can be proved as experincing havean.",
-        img: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201506/thumb-image_647_060515020243.jpg?size=770:433",
-      },
-      {
-        title: "How to Escape world with a good Trip?",
-        Author: "Anand Raghuvanshi",
-        like: 1122,
-        comments: [],
-        time: "29 May, 2022",
-        body: "So, as we all are born in this, adventure circle of 7 Hell, i.e is our life. Travelling can be proved as experincing havean.",
-        img: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201506/thumb-image_647_060515020243.jpg?size=770:433",
-      },
-      {
-        title: "How to Escape world with a good Trip?",
-        Author: "Anand Raghuvanshi",
-        like: 1122,
-        comments: [],
-        time: "29 May, 2022",
-        body: "So, as we all are born in this, adventure circle of 7 Hell, i.e is our life. Travelling can be proved as experincing havean.",
-        img: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201506/thumb-image_647_060515020243.jpg?size=770:433",
-      },
-      {
-        title: "How to Escape world with a good Trip?",
-        author: "Anand Raghuvanshi",
-        like: 1122,
-        comments: [],
-        time: "29 May, 2022",
-        body: "So, as we all are born in this, adventure circle of 7 Hell, i.e is our life. Travelling can be proved as experincing havean.",
-        img: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201506/thumb-image_647_060515020243.jpg?size=770:433",
-      },
-    ];
+    this.state = {
+      blogs: [
+        {
+          title: "How to Escape world with a good Trip?",
+          author: "Anand Raghuvanshi",
+          like: 1122,
+          comments: {
+            commentCount: 5,
+            comment: [
+              { user: "anand", body: "nice blog" },
+              { user: "ashish", body: "beautiful thought" },
+              { user: "ankit", body: "incrediable" },
+              { user: "ayush", body: "really, I didn't even know" },
+              { user: "sumit", body: "nice thought" },
+            ],
+          },
+          time: "29 May, 2022",
+          body: "So, as we all are born in this, adventure circle of 7 Hell, i.e is our life. Travelling can be proved as experincing havean.",
+          img: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201506/thumb-image_647_060515020243.jpg?size=770:433",
+          blogId: 1,
+        },
+        {
+          title: "How to Escape world with a good Trip?",
+          Author: "Anand Raghuvanshi",
+          like: 1122,
+          comments: {
+            commentCount: 5,
+            comment: [
+              { user: "anand", body: "nice blog" },
+              { user: "ashish", body: "beautiful thought" },
+              { user: "ankit", body: "incrediable" },
+              { user: "ayush", body: "really, I didn't even know" },
+              { user: "sumit", body: "nice thought" },
+            ],
+          },
+          time: "29 May, 2022",
+          body: "So, as we all are born in this, adventure circle of 7 Hell, i.e is our life. Travelling can be proved as experincing havean.",
+          img: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201506/thumb-image_647_060515020243.jpg?size=770:433",
+          blogId: 2,
+        },
+        {
+          title: "How to Escape world with a good Trip?",
+          Author: "Anand Raghuvanshi",
+          like: 1122,
+          comments: {
+            commentCount: 5,
+            comment: [
+              { user: "anand", body: "nice blog" },
+              { user: "ashish", body: "beautiful thought" },
+              { user: "ankit", body: "incrediable" },
+              { user: "ayush", body: "really, I didn't even know" },
+              { user: "sumit", body: "nice thought" },
+            ],
+          },
+          time: "29 May, 2022",
+          body: "So, as we all are born in this, adventure circle of 7 Hell, i.e is our life. Travelling can be proved as experincing havean.",
+          img: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201506/thumb-image_647_060515020243.jpg?size=770:433",
+          blogId: 3,
+        },
+        {
+          title: "How to Escape world with a good Trip?",
+          Author: "Anand Raghuvanshi",
+          like: 1122,
+          comments: {
+            commentCount: 5,
+            comment: [
+              { user: "anand", body: "nice blog" },
+              { user: "ashish", body: "beautiful thought" },
+              { user: "ankit", body: "incrediable" },
+              { user: "ayush", body: "really, I didn't even know" },
+              { user: "sumit", body: "nice thought" },
+            ],
+          },
+          time: "29 May, 2022",
+          body: "So, as we all are born in this, adventure circle of 7 Hell, i.e is our life. Travelling can be proved as experincing havean.",
+          img: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201506/thumb-image_647_060515020243.jpg?size=770:433",
+          blogId: 4,
+        },
+        {
+          title: "How to Escape world with a good Trip?",
+          author: "Anand Raghuvanshi",
+          like: 1122,
+          comments: {
+            commentCount: 5,
+            comment: [
+              { user: "anand", body: "nice blog" },
+              { user: "ashish", body: "beautiful thought" },
+              { user: "ankit", body: "incrediable" },
+              { user: "ayush", body: "really, I didn't even know" },
+              { user: "sumit", body: "nice thought" },
+            ],
+          },
+          time: "29 May, 2022",
+          body: "So, as we all are born in this, adventure circle of 7 Hell, i.e is our life. Travelling can be proved as experincing havean.",
+          img: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/201506/thumb-image_647_060515020243.jpg?size=770:433",
+          blogId: 5,
+        },
+      ],
+    };
   }
   render() {
     console.log("hello inside blogpage");
+    const handleDeleteBlog = (id) => {
+      console.log("inside deleting ", id);
+      const blogs = this.state.blogs;
+      const newBlogs = blogs.filter((item) => item.blogId !== id);
+      this.setState({
+        blogs: newBlogs,
+      });
+    };
     return (
       <div className="blog-page">
-        {this.state.map((blogData, index) => {
-          return <Blog data={blogData} key={index} />;
+        {this.state.blogs.map((blogData, index) => {
+          return (
+            <Blog
+              data={blogData}
+              key={blogData.blogId}
+              OnClickDelete={handleDeleteBlog}
+            />
+          );
         })}
       </div>
     );
